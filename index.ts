04e744fbc8914b8a8491c25c1559b32268477f29
BANDI RAJESH <bandirajesh209@gmail.com>
2025-08-17 20:23:36 +0530

    drazzle-added

--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { connectMongo } from "./mongo";
 import { scheduleDailyReminders } from "./scheduler";
-import { setupVite, serveStatic, log } from "./vite";
+
+import { log, serveStatic } from "./vite";
+// Vite setup removed; only serveStatic is used for static files
+
 
 const app = express();
 app.use(express.json());
@@ -103,11 +106,7 @@ function listRoutes(app: any) {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+  serveStatic(app);
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
